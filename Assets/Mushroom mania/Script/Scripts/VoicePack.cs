3fb3f99fd63488b09cbaf188ecab2f7bf6944842
enyaaaa <enya.tan3009@gmail.com>
2025-02-12 04:12:08 +0800

    pausemenu error not fixed

---
+++ Assets/Mushroom mania/Script/Scripts/VoicePack.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class VoicePack : MonoBehaviour
+{
+    public AudioClip[] jumpVoiceSFX;
+    public AudioClip[] specialJumpVoiceSFX;
+    public AudioClip[] dieVoiceSFX;
+    public AudioClip hurtVoiceSFX;
+    public AudioClip burnVoiceSFX;
+    public AudioClip fallVoiceSFX;
+    public AudioClip victoryVoiceSFX;
+    public AudioClip collectItemVoiceSFX;
+    public AudioClip startSFX;
+}
+

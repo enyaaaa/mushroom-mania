1f389fdc89d26d09e3faefbcb13a3b3bb28bdd67
eunicelyy <e.lee12312@gmail.com>
2025-02-13 07:58:32 +0800

    changed the coin spawner script (but dk whether it works)

--- Assets/Mushroom mania/Script/CoinSpawner.cs
+++ Assets/Mushroom mania/Script/CoinSpawner.cs
@@ -5,9 +5,11 @@ public class CoinSpawner : MonoBehaviour
     public GameObject coinPrefab; // Assign the Coin Prefab in Inspector
     public int coinCount = 10;    // Number of coins to spawn
     public Vector3 spawnArea = new Vector3(5, 1, 5); // Adjust area for spawning
+    public Transform spawnParent; // Optional: Assign a parent object for better organization
 
     void Start()
     {
+        ResetCoins(); // Ensure coins reset when the game starts
         SpawnCoins();
     }
 
@@ -16,12 +18,27 @@ public class CoinSpawner : MonoBehaviour
         for (int i = 0; i < coinCount; i++)
         {
             Vector3 randomPosition = new Vector3(
-                Random.Range(-spawnArea.x, spawnArea.x), 
-                spawnArea.y, 
-                Random.Range(-spawnArea.z, spawnArea.z)
+                Random.Range(-spawnArea.x / 2, spawnArea.x / 2),
+                spawnArea.y,
+                Random.Range(-spawnArea.z / 2, spawnArea.z / 2)
             );
 
-            Instantiate(coinPrefab, randomPosition, Quaternion.identity);
+            GameObject newCoin = Instantiate(coinPrefab, randomPosition, Quaternion.identity);
+
+            if (spawnParent != null)
+            {
+                newCoin.transform.SetParent(spawnParent); // Group spawned coins under a parent
+            }
+        }
+    }
+
+    public void ResetCoins()
+    {
+        // Remove all existing coins in the scene
+        GameObject[] existingCoins = GameObject.FindGameObjectsWithTag("Coin");
+        foreach (GameObject coin in existingCoins)
+        {
+            Destroy(coin);
         }
     }
 }

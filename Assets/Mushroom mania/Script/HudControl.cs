b68d096d67d9cf866c225299eac8372b312f6aa5
enyaaaa <enya.tan3009@gmail.com>
2025-02-13 02:38:12 +0800

    coin

--- Assets/Mushroom mania/Script/HudControl.cs
+++ Assets/Mushroom mania/Script/HudControl.cs
@@ -18,12 +18,9 @@ namespace HelloMarioFramework
 
         //Components
         [SerializeField]
-        private Text starText;
-        [SerializeField]
         private Text coinText;
 
         //Optimization
-        private int prevStar = -1;
         private int prevCoin = 0;
 #if UNITY_EDITOR
         //Null check
@@ -39,11 +36,6 @@ namespace HelloMarioFramework
             {
                 prevCoin = SaveData.save.GetCoins();
                 coinText.text = SaveData.save.GetCoins().ToString();
-            }
-            if (prevStar != SaveData.save.GetStarCount())
-            {
-                prevStar = SaveData.save.GetStarCount();
-                starText.text = SaveData.save.GetStarCount().ToString();
             }
         }
     }

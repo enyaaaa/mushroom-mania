a73bc8413446ad476c221a0107a99b7addc10e42
eunicelyy <e.lee12312@gmail.com>
2025-02-13 08:03:07 +0800

    changes in the scripts

--- Assets/Mushroom mania/Script/HideNearCamera.cs
+++ Assets/Mushroom mania/Script/HideNearCamera.cs
@@ -1,11 +1,3 @@
-/*
- *  Copyright (c) 2024 Hello Fangaming
- *
- *  Use of this source code is governed by an MIT-style
- *  license that can be found in the LICENSE file or at
- *  https://opensource.org/licenses/MIT.
- *  
- * */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- Assets/Mushroom mania/Script/Rotator.cs
+++ Assets/Mushroom mania/Script/Rotator.cs
@@ -1,12 +1,4 @@
-﻿/*
- *  Copyright (c) 2024 Hello Fangaming
- *
- *  Use of this source code is governed by an MIT-style
- *  license that can be found in the LICENSE file or at
- *  https://opensource.org/licenses/MIT.
- *  
- * */
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
